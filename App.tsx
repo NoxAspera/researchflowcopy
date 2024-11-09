2c9be6aab822eaf6e7a60da626c8289ceb0f0ac7
YUMM13 <blakestambaugh1@gmail.com>
2024-11-09 16:59:13 -0700

    Added basic navigation, clicking View past notes and add notes brings you to the site selection screen

--- App.tsx
+++ App.tsx
@@ -1,76 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import SelectSite from './components/SelectSite'
+import HomeScreen from './components/HomeScreen'
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      {/* header */}
-      <View style={styles.header}>
-        <Text style={styles.headerText}>Home</Text>
-      </View>
-
-      {/* buttons */}
-      <TouchableOpacity
-        style={[styles.homeButton, {backgroundColor: 'green'}]}
-        onPress={() => alert('Add Notes')} >
-          <Text style={styles.homeButtonText}>Add Notes</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={[styles.homeButton, {backgroundColor: 'red'}]}
-        onPress={() => alert('Bad Data')} >
-          <Text style={styles.homeButtonText}>Bad Data</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={[styles.homeButton, {backgroundColor: 'blue'}]}
-        onPress={() => alert('Tank Tracker')} >
-          <Text style={styles.homeButtonText}>Tank Tracker</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={[styles.homeButton, {backgroundColor: 'yellow'}]}
-        onPress={() => alert('Instrument Maintenence')} >
-          <Text style={styles.homeButtonText}>Instrument Maintenence</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={[styles.homeButton, {backgroundColor: 'grey'}]}
-        onPress={() => alert('Instrument Maintenence')} >
-          <Text style={styles.homeButtonText}>View Past Notes</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={[styles.homeButton, {backgroundColor: 'orange'}]}
-        onPress={() => alert('Plan a Visit')} >
-          <Text style={styles.homeButtonText}>Plan a Visit</Text>
-      </TouchableOpacity>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="SelectSite" component={SelectSite} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'stretch',        // has button fill space horizontally
-    justifyContent: 'space-evenly',
-  },
-  homeButton: {
-    flex: 1,                      // has button fill space vertically
-    borderRadius: 10,
-    justifyContent: 'center',     // this and alignItems places text in center of button
-    alignItems: "center",
-    margin: 10
-  },
-  homeButtonText: {
-    flex: 1,
-    fontSize: 30
-  },
-  header: {
-    width: '100%', // Ensure the header takes full width
-    alignItems: 'center', // Center the text horizontally
-    marginBottom: 20, // Add space between the header and the buttons
-  },
-  headerText: {
-    fontSize: 48,
-    marginTop: 30,
-  }
-});
+
---
+++ components/HomeScreen.tsx
@@ -0,0 +1,85 @@
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+export default function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      {/* header */}
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Home</Text>
+      </View>
+
+      {/* buttons */}
+      
+      {/* Select Site  */}
+      <TouchableOpacity
+        style={[styles.homeButton, {backgroundColor: 'green'}]}
+        onPress={() => navigation.navigate('SelectSite')} >
+          <Text style={styles.homeButtonText}>Add Notes</Text>
+      </TouchableOpacity>
+
+      {/* Bad Data  */}
+      <TouchableOpacity
+        style={[styles.homeButton, {backgroundColor: 'red'}]}
+        onPress={() => alert('Bad Data')} >
+          <Text style={styles.homeButtonText}>Bad Data</Text>
+      </TouchableOpacity>
+
+      {/* Tank Tracker  */}
+      <TouchableOpacity
+        style={[styles.homeButton, {backgroundColor: 'blue'}]}
+        onPress={() => alert('Tank Tracker')} >
+          <Text style={styles.homeButtonText}>Tank Tracker</Text>
+      </TouchableOpacity>
+
+      {/* Instrument Maintenence  */}
+      <TouchableOpacity
+        style={[styles.homeButton, {backgroundColor: 'yellow'}]}
+        onPress={() => alert('Instrument Maintenence')} >
+          <Text style={styles.homeButtonText}>Instrument Maintenence</Text>
+      </TouchableOpacity>
+
+      {/* View Past Notes  */}
+      <TouchableOpacity
+        style={[styles.homeButton, {backgroundColor: 'grey'}]}
+        onPress={() => navigation.navigate('SelectSite')} >
+          <Text style={styles.homeButtonText}>View Past Notes</Text>
+      </TouchableOpacity>
+
+      {/* Plan a visit  */}
+      <TouchableOpacity
+        style={[styles.homeButton, {backgroundColor: 'orange'}]}
+        onPress={() => alert('Plan a visit')} >
+          <Text style={styles.homeButtonText}>Plan a Visit</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'stretch',        // has button fill space horizontally
+    justifyContent: 'space-evenly',
+  },
+  homeButton: {
+    flex: 1,                      // has button fill space vertically
+    borderRadius: 10,
+    justifyContent: 'center',     // this and alignItems places text in center of button
+    alignItems: "center",
+    margin: 10
+  },
+  homeButtonText: {
+    flex: 1,
+    fontSize: 30
+  },
+  header: {
+    width: '100%', // Ensure the header takes full width
+    alignItems: 'center', // Center the text horizontally
+    marginBottom: 20, // Add space between the header and the buttons
+  },
+  headerText: {
+    fontSize: 48,
+    marginTop: 30,
+  }
+});
