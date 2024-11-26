759184c25201a3cd6cfe37c17294d7c0240b1028
YUMM13 <blakestambaugh1@gmail.com>
2024-11-26 16:17:33 -0700

    updated ui for add notes and view notes

--- components/AddNotes.tsx
+++ components/AddNotes.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, TouchableOpacity } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 import React, { useState } from 'react';
 import { RouteProp, useRoute } from '@react-navigation/native';
@@ -6,7 +6,7 @@ import { ScrollView } from 'react-native-gesture-handler';
 import { StackNavigationProp } from '@react-navigation/stack';
 import AddNotesTextInput from './AddNotesTextInput';
 import { NaviProp } from './types';
-import { ApplicationProvider, IndexPath, Input, Layout, Select, SelectItem, Button } from '@ui-kitten/components';
+import { ApplicationProvider, IndexPath, Input, Layout, Select, SelectItem, Button, Text } from '@ui-kitten/components';
 import * as eva from '@eva-design/eva';
 import { default as theme } from '../custom-theme.json'
 
@@ -43,8 +43,14 @@ export default function AddNotes({ navigation }: NaviProp) {
       lts = entry.lts.id || '';
     }
 
-    const [selectedValue, setSelectedValue] = useState("");
-    // Use IndexPath for selected index
+    // these use states to set and store values in the text inputs
+    const [timeValue, setTimeValue] = useState("");
+    const [lowValue, setLowValue] = useState("");
+    const [medValue, setMedValue] = useState("");
+    const [highValue, setHighValue] = useState("");
+    const [n2Value, setN2Value] = useState("");
+    const [notesValue, setNotesValue] = useState("");
+    // Use IndexPath for selected index for drop down menu
     const [selectedIndex, setSelectedIndex] = useState<IndexPath>(new IndexPath(0)); // Default to first item
     const instruments = ['Instrument 1', 'Instrument 2', 'Instrument 3']
 
@@ -52,7 +58,9 @@ export default function AddNotes({ navigation }: NaviProp) {
     return (
       <ApplicationProvider {...eva} theme={theme}>
         <Layout style={styles.container} level='1'>
-
+          {/* header */}
+          <Text category='h1' style={{textAlign: 'center'}}>{site}</Text>
+          
           {/* drop down menu for instruments */}
           <Select label='Instrument'
             selectedIndex={selectedIndex}
@@ -74,16 +82,16 @@ export default function AddNotes({ navigation }: NaviProp) {
           <Input
             label='Time'
             placeholder='12:00 PM'
-            value={selectedValue}
-            onChangeText={selectedValue => setSelectedValue(selectedValue)}
+            value={timeValue}
+            onChangeText={timeValue => setTimeValue(timeValue)}
             style={styles.label}
           />
           {/* N2 */}
           <Input
             label='N2'
             placeholder='Level'
-            value={selectedValue}
-            onChangeText={selectedValue => setSelectedValue(selectedValue)} 
+            value={n2Value}
+            onChangeText={n2Value => setN2Value(n2Value)} 
             style={styles.label}
           />
           {/* Low input */}
@@ -91,8 +99,8 @@ export default function AddNotes({ navigation }: NaviProp) {
             label='Low'
             placeholder='Level'
             caption=''
-            value={selectedValue}
-            onChangeText={selectedValue => setSelectedValue(selectedValue)} 
+            value={lowValue}
+            onChangeText={lowValue => setLowValue(lowValue)} 
             style={styles.label}
           />
           {/* mid input */}
@@ -100,8 +108,8 @@ export default function AddNotes({ navigation }: NaviProp) {
             label='Mid'
             placeholder='Level'
             caption=''
-            value={selectedValue}
-            onChangeText={selectedValue => setSelectedValue(selectedValue)} 
+            value={medValue}
+            onChangeText={medValue => setMedValue(medValue)} 
             style={styles.label}
           />
           {/* high input */}
@@ -109,19 +117,18 @@ export default function AddNotes({ navigation }: NaviProp) {
             label='High'
             placeholder='Level'
             caption=''
-            value={selectedValue}
-            onChangeText={selectedValue => setSelectedValue(selectedValue)} 
+            value={highValue}
+            onChangeText={highValue => setHighValue(highValue)} 
             style={styles.label}
           />
           {/* notes entry */}
           <Input
             label='Notes'
             placeholder='All Good.'
-            value={selectedValue}
+            value={notesValue}
             size='large'
             multiline={true}
-            onChangeText={selectedValue => setSelectedValue(selectedValue)} 
-            style={styles.label}
+            onChangeText={notesValue => setNotesValue(notesValue)} 
           />
           {/* submit button */}
           <Button
