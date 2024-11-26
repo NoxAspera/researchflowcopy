3a719e841078e80ceb519be84674cef8f96836f9
YUMM13 <blakestambaugh1@gmail.com>
2024-11-25 17:29:09 -0700

    upgraded home page and select site ui

--- components/HomeScreen.tsx
+++ components/HomeScreen.tsx
@@ -1,37 +1,86 @@
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, TouchableOpacity, ScrollView, TouchableWithoutFeedback, ImageBackground, Image } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import HomeButtonProp from './HomeButtonProp';
 import { NaviProp } from './types';
+import { ApplicationProvider, Layout, Text } from '@ui-kitten/components'
+import * as eva from '@eva-design/eva';
+import { default as theme } from '../custom-theme.json'
 
 
 export default function HomeScreen({navigation}: NaviProp) {
   return (
-    <View style={styles.container}>
-      {/* header */}
-      <View style={styles.header}>
-        <Text style={styles.headerText}>Home</Text>
-      </View>
+    <ApplicationProvider {...eva} theme={theme}>
+      <Layout style={styles.container}>
+            <ScrollView contentContainerStyle={styles.scrollContainer}>
+            {/* Add Notes */}
+            <TouchableWithoutFeedback onPress={() => navigation.navigate('SelectSite', {from: 'AddNotes'})}>
+                <Layout style={styles.tab} level="2">
+                  {/* <ImageBackground source={{uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAQWkqKNH5kgJFOJzGNeGPogFhdvMBAwtiDg&s'}} 
+                  resizeMode='cover' style={styles.imageBackground}> */}
+                    <Text category="h5">ADD NOTES</Text>
+                  {/* </ImageBackground> */}
+                </Layout>
+            </TouchableWithoutFeedback>
 
-      {/* buttons */}
+            {/* VIEW PAST NOTES */}
+            <TouchableWithoutFeedback onPress={() => navigation.navigate('SelectSite', {from: 'ViewNotes'})}>
+                <Layout style={styles.tab} level="2">
+                    <Text category="h5">VIEW PAST NOTES</Text>
+                </Layout>
+            </TouchableWithoutFeedback>
+            {/* BAD DATA */}
+            <TouchableWithoutFeedback onPress={() => navigation.navigate('SelectSite', {from: "BadData"})}>
+                <Layout style={styles.tab} level="2">
+                    <Text category="h5">BAD DATA</Text>
+                </Layout>
+            </TouchableWithoutFeedback>
+            {/* INSTRUMENT MAINTENENCE */}
+            <TouchableWithoutFeedback onPress={() => navigation.navigate('SelectInstrument')}>
+                <Layout style={styles.tab} level="2">
+                    <Text category="h5">INSTRUMENT MAINTENENCE</Text>
+                </Layout>
+            </TouchableWithoutFeedback>
+            {/* TANK TRACKER */}
+            <TouchableWithoutFeedback onPress={() => navigation.navigate('SelectTank')}>
+                <Layout style={styles.tab} level="2">
+                    <Text category="h5">TANK TRACKER</Text>
+                </Layout>
+            </TouchableWithoutFeedback>
+            {/* PLAN A VISIT */}
+            <TouchableWithoutFeedback onPress={() => alert('PLAN A VISIT')}>
+                <Layout style={styles.tab} level="2">
+                    <Text category="h5">PLAN A VISIT</Text>
+                </Layout>
+            </TouchableWithoutFeedback>
+            </ScrollView>
+        </Layout>
+    </ApplicationProvider>
+    // <View style={styles.container}>
+    //   {/* header */}
+    //   <View style={styles.header}>
+    //     <Text style={styles.headerText}>Home</Text>
+    //   </View>
+
+    //   {/* buttons */}
       
-      {/* Select Site  */}
-      <HomeButtonProp color='green' text='Add Notes' onPress={() => navigation.navigate('SelectSite', {from: 'AddNotes'})} />
+    //   {/* Select Site  */}
+    //   <HomeButtonProp color='green' text='Add Notes' onPress={() => navigation.navigate('SelectSite', {from: 'AddNotes'})} />
 
-      {/* Bad Data  */}
-      <HomeButtonProp color='red' text='Bad Data' onPress={() => navigation.navigate('SelectSite', {from: "BadData"})}/>
+    //   {/* Bad Data  */}
+    //   <HomeButtonProp color='red' text='Bad Data' onPress={() => navigation.navigate('SelectSite', {from: "BadData"})}/>
 
-      {/* Tank Tracker  */}
-      <HomeButtonProp color='blue' text='Tank Tracker' onPress={() => navigation.navigate('SelectTank')} />
+    //   {/* Tank Tracker  */}
+    //   <HomeButtonProp color='blue' text='Tank Tracker' onPress={() => navigation.navigate('SelectTank')} />
 
-      {/* Instrument Maintenence  */}
-      <HomeButtonProp color='yellow' text='Instrument Maintenence' onPress={() => navigation.navigate('SelectInstrument')} />
+    //   {/* Instrument Maintenence  */}
+    //   <HomeButtonProp color='yellow' text='Instrument Maintenence' onPress={() => navigation.navigate('SelectInstrument')} />
 
-      {/* View Past Notes  */}
-      <HomeButtonProp color='grey' text='View Past Notes' onPress={() => navigation.navigate('SelectSite', {from: 'ViewNotes'})} />
+    //   {/* View Past Notes  */}
+    //   <HomeButtonProp color='grey' text='View Past Notes' onPress={() => navigation.navigate('SelectSite', {from: 'ViewNotes'})} />
 
-      {/* Plan a visit  */}
-      <HomeButtonProp color='orange' text='Plan a Visit' onPress={() => alert('plan a visit')} />
-    </View>
+    //   {/* Plan a visit  */}
+    //   <HomeButtonProp color='orange' text='Plan a Visit' onPress={() => alert('plan a visit')} />
+    // </View>
   );
 }
 
@@ -50,5 +99,29 @@ const styles = StyleSheet.create({
   headerText: {
     fontSize: 48,
     marginTop: 30,
-  }
+  },
+  scrollContainer: {
+    paddingVertical: 16,
+    alignItems: 'center', // Center cards horizontally
+  },
+  tab: {
+    width: '90%', // Card width to adjust appearance
+    height: 200,
+    marginVertical: 10,
+    padding: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 8,
+    elevation: 4, // For shadow on Android
+    shadowColor: '#000', // For shadow on iOS
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+  },
+  imageBackground: {
+    flex: 1,
+    height: 150, // Adjust as needed for your design
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 });
--- components/SelectSite.tsx
+++ components/SelectSite.tsx
@@ -1,9 +1,12 @@
-import { StyleSheet, Text, View, Alert, Button, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Alert, TouchableOpacity } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 import React, { useState } from 'react';
 import { useRoute } from '@react-navigation/native';
 import { parseDocument } from '../parsers/AddNotesParser';
 import { StackNavigationProp } from '@react-navigation/stack';
+import { ApplicationProvider, Layout, Button } from '@ui-kitten/components';
+import * as eva from '@eva-design/eva';
+import { default as theme } from '../custom-theme.json'
 
 // Define the type for the stack's navigation parameters
 type RootStackParamList = {
@@ -24,7 +27,7 @@ export default function SelectSite({navigation}: HomeScreenProps) {
   const route = useRoute();
   let from = route.params?.from;
 
-  const handleConfirm = () => {
+  const handleConfirm = (selectedSite: string) => {
     if(from === 'AddNotes'){
         const textDocument = `
         # Site id: **sug**
@@ -47,42 +50,66 @@ export default function SelectSite({navigation}: HomeScreenProps) {
             // Parse the document
             const parsedData = parseDocument(textDocument);
 
-        navigation.navigate('AddNotes', {site: selectedValue, info: parsedData}); //{site: selectValue} tells the AddNotes what the selected value is
+        navigation.navigate('AddNotes', {site: selectedSite, info: parsedData}); //{site: selectValue} tells the AddNotes what the selected value is
         }
     else if(from === 'ViewNotes'){
-        navigation.navigate('ViewNotes', {site: selectedValue}); //{site: selectValue} tells the AddNotes what the selected value is
+        navigation.navigate('ViewNotes', {site: selectedSite}); //{site: selectValue} tells the AddNotes what the selected value is
         }
     else if(from === 'BadData'){
-        navigation.navigate('BadData', {site: selectedValue}); //{site: selectValue} tells the AddNotes what the selected value is
+        navigation.navigate('BadData', {site: selectedSite}); //{site: selectValue} tells the AddNotes what the selected value is
         }
   };
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.dropdownContainer}>
-        <Text style={styles.label}>Where are you?</Text>
-        <Picker
-          selectedValue={selectedValue}
-          onValueChange={(itemValue: React.SetStateAction<string>) => setSelectedValue(itemValue)}
-          style={styles.picker}
-        >
-          <Picker.Item label="CSP" value="CSP" />
-          <Picker.Item label="DBK" value="DBK" />
-          <Picker.Item label="FRU" value="FRU" />
-          <Picker.Item label="HDP" value="HDP" />
-          <Picker.Item label="HPL" value="HPL" />
-          <Picker.Item label="RPK" value="RPK" />
-          <Picker.Item label="SUG" value="SUG" />
-          <Picker.Item label="WBB" value="WBB" />
-        </Picker>
-      </View>
+  const buttonData = [
+    { id: 1, label: 'CSP', onPress: () => handleConfirm('CSP')},
+    { id: 2, label: 'DBK', onPress: () => handleConfirm('DBK')},
+    { id: 3, label: 'FRU', onPress: () => handleConfirm('FRU')},
+    { id: 4, label: 'HDP', onPress: () => handleConfirm('HDP')},
+    { id: 5, label: 'HPL', onPress: () => handleConfirm('HPL')},
+    { id: 6, label: 'RPK', onPress: () => handleConfirm('RPK')},
+    { id: 7, label: 'SUG', onPress: () => handleConfirm('SUG')},
+    { id: 8, label: 'WBB', onPress: () => handleConfirm('WBB')}
+  ]
 
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.button} onPress={() => handleConfirm()}>
-          <Text style={styles.buttonText}>Confirm</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
+  return (
+    <ApplicationProvider {...eva} theme={theme}>
+      <Layout style={styles.container}>
+        {buttonData.map((button) => (
+          <Button
+            key={button.id}
+            style={styles.button}
+            onPress={button.onPress}
+          >
+            {button.label}
+          </Button>
+        ))}
+      </Layout>
+    </ApplicationProvider>
+    // <View style={styles.container}>
+    //   <View style={styles.dropdownContainer}>
+    //     <Text style={styles.label}>Where are you?</Text>
+    //     <Picker
+    //       selectedValue={selectedValue}
+    //       onValueChange={(itemValue: React.SetStateAction<string>) => setSelectedValue(itemValue)}
+    //       style={styles.picker}
+    //     >
+    //       <Picker.Item label="CSP" value="CSP" />
+    //       <Picker.Item label="DBK" value="DBK" />
+    //       <Picker.Item label="FRU" value="FRU" />
+    //       <Picker.Item label="HDP" value="HDP" />
+    //       <Picker.Item label="HPL" value="HPL" />
+    //       <Picker.Item label="RPK" value="RPK" />
+    //       <Picker.Item label="SUG" value="SUG" />
+    //       <Picker.Item label="WBB" value="WBB" />
+    //     </Picker>
+    //   </View>
+
+    //   <View style={styles.buttonContainer}>
+    //     <TouchableOpacity style={styles.button} onPress={() => handleConfirm()}>
+    //       <Text style={styles.buttonText}>Confirm</Text>
+    //     </TouchableOpacity>
+    //   </View>
+    // </View>
   );
 }
 
