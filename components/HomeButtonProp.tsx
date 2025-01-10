d6132b99060881f1dd076d9219d017cbe93f6eb2
SamTheTiger03 <schiwaldavid@gmail.com>
2025-01-09 18:48:29 -0700

    converted home buttons too ui-kitten buttons so hopefully I can import the style sheet for making them different colors

--- components/HomeButtonProp.tsx
+++ components/HomeButtonProp.tsx
@@ -5,23 +5,34 @@
  * 
  * This property abstracts the buttons on the home page
  */
-import { Layout, Text } from '@ui-kitten/components';
+import { Layout, Text, Button } from '@ui-kitten/components';
 import React from 'react'
 import { StyleSheet, TouchableWithoutFeedback, Dimensions } from "react-native"
 const { width, height } = Dimensions.get("window"); //this pulls in the screen width and height to use for scalars
 
 interface HBProp{
     text: string;
+    //style: StyleSheet;
     onPress: () => void; // may change depending on the function pass in
 }
-
+//might be able to pass in the style of the button from outside to enable different colors for the buttons
 const HomeButtonProp: React.FC<HBProp> = ({ text, onPress }) => {
     return (
       <TouchableWithoutFeedback onPress={onPress}>
-        <Layout style={styles.tab} level="2">
-          <Text category="h5">{text}</Text>
+        <Layout level="2">
+          <Button
+            style={styles.tab}
+            status='primary'
+          >
+            {evaProps => <Text {...evaProps} category="h5" style={{color: "white"}}>{text}</Text>}
+          </Button>  
         </Layout>
       </TouchableWithoutFeedback>
+      /*<TouchableWithoutFeedback onPress={onPress}>
+        <Layout {...style} level="2">
+          <Text category="h5">{text}</Text>  
+        </Layout>
+      </TouchableWithoutFeedback>*/
     );
 };
 
@@ -39,6 +50,7 @@ const styles = StyleSheet.create({
       shadowOffset: { width: 0, height: 2 },
       shadowOpacity: 0.2,
       shadowRadius: 4,
+      //backgroundColor: "#061c9c" //changes color of all buttons
     }
   });
 
