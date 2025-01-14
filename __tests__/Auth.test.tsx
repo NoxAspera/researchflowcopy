d394b28ff15140390de740b8ccaf7f1dac5ed29d
YUMM13 <blakestambaugh1@gmail.com>
2025-01-13 18:11:31 -0700

    hopefully everything works. One test is working

--- __tests__/Auth.test.tsx
+++ __tests__/Auth.test.tsx
@@ -1,9 +1,8 @@
 import Auth from '../components/Auth';
-import React from 'react';
-// import NavigationType from '../components/types'
 import { render } from '@testing-library/react-native'
 import { NaviProp, RootStackParamList } from '../components/types';
-import { StackNavigationProp } from '@react-navigation/stack';
+import React from 'react';
+
 
 const rootStack: RootStackParamList = {
   SelectSite: {
@@ -50,39 +49,18 @@ const mockNavigation: NaviProp = {
   popToTop: jest.fn(), 
   popTo: jest.fn()
 }
-// const mockNaviProp: StackNavigationProp<RootStackParamList, 'SelectSite'> = {
-//   // Mocked navigation methods
-//   navigate: jest.fn(),
-//   goBack: jest.fn(),
-//   reset: jest.fn(),
-//   setParams: jest.fn(),
-//   dispatch: jest.fn(),
-//   isFocused: jest.fn(() => true),
-//   canGoBack: jest.fn(() => true),
-//   getParent: jest.fn(),
 
-//   // Stack-specific methods
-//   push: jest.fn(),
-//   replace: jest.fn(),
-//   pop: jest.fn(),
-//   popToTop: jest.fn(),
+jest.mock('@react-navigation/native', () => ({
+  ...jest.requireActual('@react-navigation/native'),
+  useNavigation: () => ({
+    navigate: jest.fn(),
+  }),
+  useRoute: () => ({
+    params: {},
+  }),
+}));
 
-//   // Event listener methods
-//   addListener: jest.fn(),
-//   removeListener: jest.fn(),
-
-//   // Optional methods
-//   getState: jest.fn(),
-//   // getCurrentRoute: jest.fn(),
-//   // emit: jest.fn(),
-  
-//   navigateDeprecated: jest.fn(), // Not commonly used, but required for type
-//   preload: jest.fn(), // Preload a route, rarely used
-//   getId: jest.fn(), // Retrieve the ID of the current route
-//   setStateForNextRouteNamesChange: jest.fn(), // Update state for route name changes
-// };
-
-describe('<App />', () => {
+describe('Login page', () => {
   test('app renders correctly', () => {
     const { getByText } = render(<Auth navigation={mockNavigation} />);
     getByText('Sign in using your GitHub credentials');
