d81464ae651be030b79a592f5f0c2f203c02312e
NoxAspera <noxaspera@Mac.Home>
2025-02-25 21:10:07 -0700

    fixing some issues

--- App.tsx
+++ App.tsx
@@ -21,6 +21,7 @@ import SelectTank from './components/SelectTank';
 import customColors from './custom-theme.json'
 import { useState } from 'react';
 import SettingsButton from './components/SettingsButton';
+import CalendarScreen from './components/Calendar';
 
 const Stack = createStackNavigator();
 type ThemeType = 'light' | 'dark';
@@ -57,7 +58,7 @@ export default function App() {
             <Stack.Screen name="SelectTank" component={SelectTank} options={{ headerRight: () => <SettingsButton/>}}/>
             <Stack.Screen name="AddNotesMobile" component={AddNotesMobile} options={{ headerRight: () => <SettingsButton/>}}/>
             <Stack.Screen name="SelectNotes" component={SelectNotes} options={{ headerRight: () => <SettingsButton/>}}/>
-            <Stack.Screen name ="Calendar" component={Calendar} options={{headerRight: () => <SettingsButton/>}}/>
+            <Stack.Screen name ="Calendar" component={CalendarScreen} options={{headerRight: () => <SettingsButton/>}}/>
           </Stack.Navigator>
         </NavigationContainer>
       </ApplicationProvider>
