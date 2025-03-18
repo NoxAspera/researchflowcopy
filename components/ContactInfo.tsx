11744f82913d05ddfbec8f50120649082817565f
Mostlie <megostlie@gmail.com>
2025-03-18 12:55:42 -0600

    Added links to phone and map

--- components/ContactInfo.tsx
+++ components/ContactInfo.tsx
@@ -5,17 +5,38 @@
 import { useRoute } from '@react-navigation/native';
 import { Layout, Text } from '@ui-kitten/components';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Linking, TouchableOpacity, Platform } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
 import { NavigationType, routeProp } from './types'
 
 const contacts = [
     {id: "1", name: "Maria Garcia", phone: "+18017923277"},
-    {id: "2", name: "Megan Ostlie", phone: "+18015890602"}
+    {id: "2", name: "Megan Ostlie", phone: "+18015890602"},
+    {id: "3", name: "John Lin", phone: "+18013490831"},
+    {id: "4", name: "Atmos Office", phone: "+18015816136"},
+]
+
+const hospitals = [
+  {id: "1", name: "Salt Lake City", address: "50 Medical Dr N, Salt Lake City, UT 84132"},
+  {id: "2", name: "Vernal", address: "150 W 100 N, Vernal, UT 84078"}
 ]
 
 export default function ContactInfo({ navigation }: NavigationType) {
   const route = useRoute<routeProp>();
+
+  const callContact = (phone) => {
+    Linking.openURL(`tel:${phone}`);
+  };
+
+  const openMaps = (address: string) => {
+    const encodedAddress = encodeURIComponent(address);
+  
+    const url = Platform.OS === 'ios'
+      ? `maps://maps.apple.com/?q=${encodedAddress}`  // Apple Maps for iOS
+      : `https://www.google.com/maps/search/?api=1&query=${encodedAddress}`; // Google Maps for Android
+
+    Linking.openURL(url);
+  }
 
   return (
       <Layout style={styles.container} level="1">
@@ -26,9 +47,31 @@ export default function ContactInfo({ navigation }: NavigationType) {
             data={contacts}
             keyExtractor={(item) => item.id}
             renderItem={({ item }) => (
-                <View style={styles.rowContainer}>
+                <View style={styles.listContainer}>
                 <Text>{item.name}</Text>
-                <Text>{item.phone}</Text>
+                <TouchableOpacity
+                  onPress={() => callContact(item.phone)}
+                >
+                  <Text style={styles.phoneText}>{item.phone}</Text>
+                </TouchableOpacity>
+                </View>
+            )}
+        />
+
+        <Text style={styles.headerText}>
+          Hospitals
+        </Text>
+        <FlatList
+            data={hospitals}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+                <View style={styles.listContainer}>
+                <Text>{item.name}:</Text>
+                <TouchableOpacity
+                  onPress={() => openMaps(item.address)}
+                >
+                <Text style={styles.phoneText}>{item.address}</Text>
+                </TouchableOpacity>
                 </View>
             )}
         />
@@ -39,8 +82,8 @@ export default function ContactInfo({ navigation }: NavigationType) {
   const styles = StyleSheet.create({
     container: {
       flex: 1,
-      alignItems: 'stretch',        // has button fill space horizontally
-      justifyContent: 'space-evenly',
+      alignItems: 'center',        // has button fill space horizontally
+      justifyContent: 'center',
     },
     scrollContainer: {
       backgroundColor: '#1C2760'
@@ -56,7 +99,8 @@ export default function ContactInfo({ navigation }: NavigationType) {
       flex: 1,
       flexDirection: 'row',
       alignItems: 'flex-start',        // has button fill space horizontally
-      justifyContent: 'space-evenly',
+      justifyContent: 'space-around',
+      marginBottom: 10
     },
     header: {
       margin: 10,
@@ -66,8 +110,16 @@ export default function ContactInfo({ navigation }: NavigationType) {
     },
     headerText: {
       fontSize: 34,
-      marginTop: 30,
-      marginBottom: 30,
-      paddingHorizontal: 50
+      padding: 20
+    },
+    phoneText: {
+      textDecorationLine: 'underline',
+      color: 'blue'
+    },
+    listContainer: {
+      flex: 1, 
+      flexDirection: 'column', 
+      alignItems: 'center', 
+      padding: 10
     }
 });
