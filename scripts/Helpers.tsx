8c1732cce0b9cc90075d1042d50ff5364e469761
NoxAspera <noxaspera@Mac.Home>
2025-04-12 19:42:03 -0600

    adding some comments that got lost in the merge

--- scripts/Helpers.tsx
+++ scripts/Helpers.tsx
@@ -2,13 +2,25 @@
  * This file contains some commonly used helper functions.
  */
 import * as Network from 'expo-network'
-
+/**
+ * This is a small helper function to check the connection status of the phone that is in use
+ * @author August O'Rourke
+ *
+ * @returns bool
+ */
 export async function isConnected()
 {
   let check = (await Network.getNetworkStateAsync()).isConnected
   return check
 }
 
+/**
+* Helper function that gets the time between two dates expressed as days
+* @author Blake Stambaugh
+* @param date1 first date
+* @param date2 second date
+* @returns time passed between date1 and date2 in days
+*/
 export function getTimeBetweenDates(date1, date2) {
   const timeDiffMs = Math.abs(date2.getTime() - date1.getTime());
   const seconds = Math.floor(timeDiffMs / 1000);
@@ -25,6 +37,17 @@ export function getTimeBetweenDates(date1, date2) {
   };
 }
 
+/**
+* @author Blake Stambaugh 
+* 
+* Takes in a tanks previous pressure, previous date checked, and its current pressure and estimates the 
+* amount of days remaining until it is empty.
+* 
+* @param prevPress the tanks previous pressure
+* @param prevDate the last date the tank was visited
+* @param currPress the current pressure of the tank
+* @returns how many days are left until the tank is expected to be empty
+*/
 export function daysUntilEmpty(prevPress, prevDate, currPress, currDate) {
   // get change of pressure over time, assume it is linear
   let changeOfPress = currPress - prevPress;
